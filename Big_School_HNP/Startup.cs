85b54dfbc22ac1466bcb2e15b9a1e72c5a0ac38b
hongnghiphung <45985880+hongnghiphung@users.noreply.github.com>
2019-03-29 02:35:12 +0700

    Add project files.

---
+++ Big_School_HNP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Big_School_HNP.Startup))]
+namespace Big_School_HNP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
