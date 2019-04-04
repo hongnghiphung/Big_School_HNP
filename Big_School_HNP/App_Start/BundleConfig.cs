fa38bc83847bbd9520cce238d2d648fce36611c3
hongnghiphung <45985880+hongnghiphung@users.noreply.github.com>
2019-04-04 11:04:27 +0700

    version 7
    version 7

--- Big_School_HNP/App_Start/BundleConfig.cs
+++ Big_School_HNP/App_Start/BundleConfig.cs
@@ -20,9 +20,9 @@ namespace Big_School_HNP
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox/bootbox.min.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js",                     
+                      "~/Scripts/respond.js",
+                       "~/Scripts/bootbox/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
