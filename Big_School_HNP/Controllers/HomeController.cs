dd9f849fb6957576fd645867d0378d30104d5995
hongnghiphung <45985880+hongnghiphung@users.noreply.github.com>
2019-04-04 10:42:23 +0700

    Version 5
    Version 5

--- Big_School_HNP/Controllers/HomeController.cs
+++ Big_School_HNP/Controllers/HomeController.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using Big_School_HNP.Models;
+using Big_School_HNP.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace Big_School_HNP.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+
+            var viewModel = new CoursesViewModel
+            {
+                UpcomingCourses = upcomingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
